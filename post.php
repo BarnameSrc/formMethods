a9afaab3c5ea481b40cb48c4c34d7f353152416d
BarnameOte <41343862+BarnameOte@users.noreply.github.com>
2018-07-18 14:07:57 +0430

    Final commit

--- post.php
+++ post.php
@@ -1,1 +1,20 @@
+<html>
+	<head>
+		<title>POST</title>
+	</head>
+<?php
+// Saving posted data to a variable
+$fn = $_POST['FN'];
+$ln = $_POST['LN'];
+// Printing data
+echo "Your name is " . $fn . $ln . "<br><br>";
 
+// Notes
+echo "همانطور که دیدید در این متد اطلاعات گرفته شده بدون هیچگونه درز اطلاعات در این فایل استفاده شد<br>";
+echo "حالا برای ادامه آموزش به صفحه اصلی برگردید<br>";
+?>
+	<body>
+		<br>
+		<a href="index.php"><input type="button" name="button" value="index.php"></a>
+	</body>
+</html>
