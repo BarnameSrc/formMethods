c6c98d791998dc84f87a3ebc815e78a804d97fd5
BarnameOte <41343862+BarnameOte@users.noreply.github.com>
2018-07-18 12:58:33 +0430

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-
+<html>
+  <head>
+    <title>Forms</title>
+  </head>
+  <body>
+    <form action="post.php" method="post">
+      <h1> POST method </h1><br>
+      First Name: <input type="text" name="FN"><br>
+      Last Name: <input type="text" name="LN"><br>
+      <input type="submit" name="submit" value="Submit!">
+    </form><br><br><br><br>
+    <form action="get.php" method="get">
+      <h1> GET method </h1><br>
+      First Name: <input type="text" name="FN"><br>
+      Last Name: <input type="text" name="LN"><br>
+      <input type="submit" name="submit" value="Submit!">
+    </form>
+  </body
+</html>
